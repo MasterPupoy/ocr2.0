deb07689cf3217e4c2997fbb6293668976c6ee18
jaysencarlo13 <jaysencarlo13@gmail.com>
2021-09-17 06:57:02 +0800

    Testing commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
-
 const appRoutes = require('./route/route');
-
+const path =require('path')
 const PORT = 5000;
-
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/file', express.static(path.join(__dirname,'uploads')));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', appRoutes);
 
--- route/route.js
+++ route/route.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
 const pdfController = require('../controller/controller');
-
 // const { MongoClient } = require('mongodb')
-
 const path = require('path');
-
+const uniqid = require('uniqid');
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../uploads/')
+    cb(null, path.join(__dirname,'..','uploads') )
   },
   filename: function (req, file, cb){
-    cb(null, file.fieldname + '-' + Date.now() + path.ext)
+    cb(null, file.fieldname + '-' + Date.now() + '-' + uniqid() + path.extname(file.originalname))
   }
 });
 const upload = multer({ storage : storage });
 
-router.post('/id/:id/resume/:name', upload.single('resume'), async (req, res) => {
+router.post('/save-pdf', upload.single('resume') , async (req, res) => {
+  try{
+    // console.log(req.file)
+    pdfController.pdfParser(req).then(response => res.send(response))
+    // res.json({path: req.file.path})
+  }catch(e){
+    console.log(e)
+  }
+}); 
 
-  pdfController.pdfParser(req).then(response => res.send(response))
-  
-});
-
-router.get('/:id', (req, res) => {
-
-})
+module.exports = router;
